614d8b64e4b3c01017000f1e68a115b156c0db6d
dmikab <dmikab@greeny.cs.tlu.ee>
2016-02-25 13:36:25 +0200

    classwork

--- app.php
+++ app.php
@@ -1,19 +1,25 @@
 <?php
+//require another php file //
+//../../.. 3 folders back
+require_once("../../config.php");
+
+
+$everything_was_okay = true;
+
 
 //**************
 //to field validation
 //******************
 if(isset($_GET["to"])){
-	
-	
-	
 	if(empty($_GET["to"])){
-		//it is empty
+		$everything_was_okay = false;//it is empty
+		
 		echo "Please enter the recipient!";
 		
 		}else{
 			//its not empty
 		echo "to: ".$_GET["to"]."<br>";
+		$everything_was_okay = true;
 	}
 }
 
@@ -24,11 +30,13 @@ if(isset($_GET["from"])){
 	
 	if(empty($_GET["from"])){
 		//it is empty
+		$everything_was_okay = false;
 		echo "Please enter the recipient!";
 		
 		}else{
 			//its not empty
 		echo "from: ".$_GET["from"]."<br>";
+		$everything_was_okay = true;
 	}
 }
 
@@ -40,11 +48,13 @@ if(isset($_GET["message"])){
 	
 	if(empty($_GET["message"])){
 		//it is empty
+		$everything_was_okay = false;
 		echo "Please enter the message!";
 		
 		}else{
 			//its not empty
 		echo "Message: ".$_GET["message"]."<br>";
+		$everything_was_okay = true;
 	}
 	
 	
@@ -57,16 +67,59 @@ if(isset($_GET["message"])){
 
 //Getting the message from address
 
-$my_message = $_GET["message"];
-$to = $_GET["to"];
-$from = $_GET["from"];
+//$my_message = $_GET["message"];
+//$to = $_GET["to"];
+//$from = $_GET["from"];
 
 //echo "My message is ".$my_message." and is to ".$to." and is from ".$from;
 
+/*********************************
+*************Save to DB**********
+************************************/
 
 
-
-
+// ? was everything okay
+if($everything_was_okay == true) {
+	
+	echo "Saving to database ...";
+	
+	
+	//connection with username and password
+	//access username from config
+	//echo $db_username;
+	
+	// 1 servername
+	// 2 username
+	// 3 password
+	// 4 database
+	
+	
+	$mysql = new mysqli("localhost", $db_username, $db_password, "webpr2016_dmikab");
+	
+	$stmt = $mysql->prepare("INSERT INTO messages_sample (recipient, message) VALUES (?, ?)" );
+	
+	echo $stmt->error;
+	
+	//we are replacing question marks with values
+	//s - string, data or smth that is based on characters and
+	//i - intiger, number
+	// d - decimal, float
+	$stmt->bind_param("ss",$_GET["to"], $_GET["message"] );
+	
+	//save
+	
+	if($stmt->execute()){
+		
+	echo	"saved succesefully";
+	}else{
+		
+		echo $stmt->error;
+		
+		
+		}	
+	
+	
+	}
 
 ?>
 
